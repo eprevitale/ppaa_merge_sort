0853c47b28a469fa467422a05f187774fb783eae
eprevitale <eprevitale@gmail.com>
2024-11-25 19:37:15 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn divide(list: &mut Vec<i8>) -> &mut Vec<i8> {
+    if list.len() <= 1 {
+        return list;
+    } else {
+        return list;
+    }
+}
+
+fn main() {
+    let mut list: [i8; 10] = [1,2,3,4,5,6,7,8,9,10];
+
+}
